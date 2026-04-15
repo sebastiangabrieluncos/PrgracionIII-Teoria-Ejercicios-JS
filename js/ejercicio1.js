b9b191f895dbfde5963ea64156847d7e62c96f27
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-15 08:11:15 -0300

    se crea la estructuta basica HTML y se resuelve el ejercicio 1 js

---
+++ js/ejercicio1.js
@@ -0,0 +1,7 @@
+/* Ejercicio 1: if simple
+Declara una variable edad y muestra por consola "Es mayor de edad" si es mayor o igual a 18.*/
+
+let edad = prompt("Ingrese la edad: ")
+if(edad >= "18"){
+    console.log("Es mayor de edad")
+}
