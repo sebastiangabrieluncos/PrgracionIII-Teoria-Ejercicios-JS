a8da99b67614966bd6e2d88f72763607d97ecd32
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-15 08:53:28 -0300

    se agrega el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,6 @@
+/*Ejercicio 6: Uso de booleanos para asignar un valor
+Declara una variable edad y otra variable esMayor que sea true si la edad es mayor o igual a 18, o false en caso contrario. Muestra el valor de esMayor por consola.*/
+
+let edad =prompt('Ingrese su edad');
+let esMayor = edad>="18";
+console.log(esMayor);
