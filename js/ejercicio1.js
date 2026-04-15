12f37309d2823411d76191d71caaf430fa9e958c
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-15 08:19:44 -0300

    se resuelve el ejercicio 2

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,7 +1,9 @@
 /* Ejercicio 1: if simple
 Declara una variable edad y muestra por consola "Es mayor de edad" si es mayor o igual a 18.*/
 
-let edad = prompt("Ingrese la edad: ")
+let edad = prompt("Ingrese la edad: ");
 if(edad >= "18"){
-    console.log("Es mayor de edad")
+    console.log("Es mayor de edad");
 }
+
+//se aclara que al usar el promt me devulve un string no un numero
---
+++ js/ejercicio2.js
@@ -0,0 +1,9 @@
+/*Ejercicio 2: if...else
+Declara una variable nota y muestra "Aprobado" si es mayor o igual a 6, y "Desaprobado" en caso contrario.*/
+
+let nota = prompt('Ingrese la nota del alumno:');
+if (nota >="6"){
+    console.log('El alumno/a esta aprobado/a');
+}else{
+    console.log('El alumno/a esta desaprobado/a');
+}
