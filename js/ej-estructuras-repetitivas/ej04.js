cda2911378e64cbffe2dbd734d305497e02068d9
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-19 12:19:27 -0300

    feat se agrega resolucion del ejercicio 4 estructuras repetitivas

---
+++ js/ej-estructuras-repetitivas/ej04.js
@@ -0,0 +1,6 @@
+//Crea un array animales con "perro", "gato" y "loro". Usa for...of para mostrar cada animal por consola.
+
+let animales= ['perro', 'gato', 'loro']
+for (let i of animales) {
+    console.log(i)
+}
